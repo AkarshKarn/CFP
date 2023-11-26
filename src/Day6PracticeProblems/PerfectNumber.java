8f5275bb02b73451b4f58af893bc4847759cc14c
AkarshKarn <akkikarn20@gmail.com>
2023-11-27 00:16:42 +0530

    [Akarsh] Added: Perfect Number program added to the branch.

---
+++ src/Day6PracticeProblems/PerfectNumber.java
@@ -0,0 +1,26 @@
+package Day6PracticeProblems;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        long n,sum=0;
+        n=sc.nextLong();
+
+        int i=1;
+
+        while (i<=n/2){
+            if (n%i==0) {
+                sum = sum + i;
+            }
+            i++;
+        }
+        if (sum==n){
+            System.out.println(n+" "+"is a perfect number");
+        }else {
+            System.out.println(n+" "+"is not a perfect number");
+        }
+    }
+}
