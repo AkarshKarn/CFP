45a17603efd5d1111a9a63cac36a964d7df6be6b
AkarshKarn <akkikarn20@gmail.com>
2023-11-27 00:21:46 +0530

    [Akarsh] Added: Reverse Number program added to the branch.

---
+++ src/Day6PracticeProblems/ReverseNumber.java
@@ -0,0 +1,22 @@
+package Day6PracticeProblems;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the number:");
+        int num=sc.nextInt();
+        int rev=0,rem=0;
+
+
+        while (num!=0){
+            rem=num%10;
+            rev=rev*10+rem;
+            num=num/10;
+        }
+        System.out.println();
+        System.out.println("After reversing the number:"+" "+rev);
+    }
+}
